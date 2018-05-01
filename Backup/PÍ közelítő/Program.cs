fc7b0ceaff7f0b1b4eabdecb16aaf5aef19e6d5b
Kukac <thekukac9@gmail.com>
2018-05-01 03:52:37 +0200

    project comaptibility update

---
+++ Backup/PÍ közelítő/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 1; // Leibniz-féle sor  // elég lassú
+           double egy = 1;
+           a = 1 - (egy / 3);
+            //decimal q = 10000000000000000000;
+
+           for (int i = 5; ; i += 2)
+           {
+               a += egy / i;
+              i += 2;
+               a -= egy / i;
+
+               
+               Console.WriteLine(Convert.ToDouble(a * 4));
+               //Console.ReadKey();*/
+               //Console.ReadKey();
+           }
+            //Console.WriteLine(a * 4);
+            
+        }
+    }
+}
